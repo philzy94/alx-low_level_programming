b77513e21af0389214f9fa313d18abba3dcab3fe
Philemon Shekari <holyphilzy@gmail.com>
2022-10-14 03:33:55 +0100

    'update'

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+  *main-runs teh main code
+  *
+  *Return: 1 if program completes
+  */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
+}
